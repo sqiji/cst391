3646314a8124f821edb48400e1d2f1ad1562553a
sqiji <sqiji@my.gcu.edu>
2024-02-03 01:51:19 -0700

    updating MusicAPI

--- MusicAPI/src/artists/artists.controller.ts
+++ MusicAPI/src/artists/artists.controller.ts
@@ -1,11 +1,14 @@
-import { Request, Response } from 'express';
 
-const ARTIST = [
-    {id: 1, name: 'The Beatles'},
-    {id: 2, name: 'The Who'},
-    {id: 3, name: 'Abba'}
-];
-
-export const getArtists = (req: Request, res: Response) => {
-    res.send(ARTIST);
+import { Request, RequestHandler, Response } from "express";
+import * as ArtistDao from './artists.dao';
+export const readArtists: RequestHandler = async (req: Request, res: Response) => {
+    try {
+        const artists = await ArtistDao.readArtists();
+        res.status(200).json(artists);
+    } catch (error) {
+        console.error('[artists.controller][ReadArtists][Error] ', error);
+        res.status(500).json({
+            message: 'There was an error when fetching artists'
+        });
+    }
 };
